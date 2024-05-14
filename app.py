7ce37410d88b17fecaea388b0412519d46989e3d
Aditya Patel <94279353+deviladii@users.noreply.github.com>
2024-05-14 11:55:01 +0530

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html'display = 'True')
 
 @app.route('/process_form', methods=['POST'])
 def process_form():
